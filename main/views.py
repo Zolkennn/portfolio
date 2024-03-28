20e66f51c6badaea370cf48446789e19b8f262e8
zolkenn <juhdshudshjd@sdjdsh.dhdg>
2024-03-28 15:15:10 +0100

    la page lit la base de donné

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+from main.models import Article
+
 
 # Create your views here.
+def index(request):
+    return render(request, "base.html", {"articles": Article.objects.all()})
