e8899ed46b29a08eb0ffa8669033ef4c3c4d3c6b
Your Name <you@example.com>
2024-04-03 11:14:06 +0200

    ajoue de la base du site en HTML et CSS + extention de la BD

--- main/models.py
+++ main/models.py
@@ -6,3 +6,4 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     tags = models.CharField(max_length=24)
+    lien = models.CharField(max_length=256)
--- main/views.py
+++ main/views.py
@@ -5,4 +5,4 @@ from main.models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, "base.html", {"articles": Article.objects.all()})
+    return render(request, "main.html", {"articles": Article.objects.all()})
